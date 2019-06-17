a917272da48c9057351fc66def5711875c4b9229
FSWL <fswl98@gmail.com>
2019-06-17 22:38:58 +0700

    Added and registered Item Component

--- Vu/src/router/index.js
+++ Vu/src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import CircleProgress from 'vue2-circle-progress'
+import ItemComponent from '@/components/ItemComponent'
 
+Vue.component('item-component', ItemComponent)
+Vue.component('circle-progress', CircleProgress)
 Vue.use(Router)
 
 export default new Router({
