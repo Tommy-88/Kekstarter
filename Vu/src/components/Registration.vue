62a089150f3c4ab5ea9b91ad194208bd8282303d
id0ntmind <n0thingss@yandex.ru>
2019-06-23 18:48:46 +0300

    add adaptive registration page

--- Vu/src/components/Registration.vue
+++ Vu/src/components/Registration.vue
@@ -1,7 +1,12 @@
 <template>
     <v-form>
+      <div class="container-fluid">
+      <div class="row justify-content-center">
       <h2>Регистрация</h2>
+      </div>
       <v-layout align-center column>
+        <div class="row justify-content-center">
+        <div class="col-md-7 col-sm-7 col-lg-8 text-center">
         <v-text-field
         v-model="login"
         label='Login'
@@ -9,18 +14,27 @@
         @input="$v.login.$touch()"
         @blur="$v.login.$touch()"
         ></v-text-field>
+        </div>
+        <div class="col-md-7 col-sm-7 col-lg-8 text-center">
         <v-text-field
           v-model="email"
           label="E-mail"
           required
         ></v-text-field>
+        </div>
+        <div class="col-md-7 col-sm-7 col-lg-8 text-center">
         <v-text-field
           v-model="password"
           label="Password"
           required
           ></v-text-field>
+        </div>
+        <div class="col-md-7 col-sm-7 col-lg-8 text-center">
         <v-btn>Регистрация</v-btn>
+        </div>
+        </div>
       </v-layout>
+      </div>
     </v-form>
 </template>
 
