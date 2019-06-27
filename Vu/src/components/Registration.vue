5e129b6ac28c5911db94621ee1e62152e5f5fe98
lerese58 <lerese58@gmail.com>
2019-06-27 13:30:48 +0300

    add register validaion

--- Vu/src/components/Registration.vue
+++ Vu/src/components/Registration.vue
@@ -3,22 +3,25 @@
     <form>
       <h2>Регистрация</h2>
       <v-layout align-center column>
-        <v-text-field
+        <v-text-field class="textField"
         v-model="login"
+        :rules="loginRules"
         label='Login'
         type="login"
         required
-        @input="$v.login.$touch()"
-        @blur="$v.login.$touch()"
         ></v-text-field>
-        <v-text-field
+<!--        @input="$v.login.$touch()"-->
+<!--        @blur="$v.login.$touch()"-->
+        <v-text-field class="textField"
           v-model="email"
-          label="email"
+          :rules="emailRules"
+          label="E-mail"
           type="email"
           required
         ></v-text-field>
-        <v-text-field
+        <v-text-field class="textField"
           v-model="password"
+          :rules="pswdRules"
           label="Password"
           type="password"
           required
@@ -33,8 +36,29 @@
   import axios from 'axios'
     export default {
         name: "Registration",
+      data () {
+          return {
+            login: '',
+            loginRules: [
+              v => !!v || 'Это поле обязательно',
+              v => v.length > 4 || 'Слишком короткий пароль',
+              v => !!v.match(/'^[a-zA-Z][a-zA-Z0-9-_]{1,40}$'/) || 'Логин может содержать буквы, и цифры и символы \'_\', \'-\''
+            ],
+            email: '',
+            emailRules: [
+              v => !!v || 'Это поле обязательно',
+              v => !!v.match(/.+@.+\..+/i) || 'Некорректный e-mail'
+            ],
+            password: '',
+            pswdRules: [
+              v => !!v || 'Это поле обязательно',
+              v => v.length > 4 || 'Слишком короткий пароль'
+            ],
+          }
+      },
       methods: {
           registerUser: function () {
+            if (!this.$refs.form.validate()) return
             axios.post('http://95.179.136.92/api/v1/user/create', {
               email: this.email,
               name: this.login,
@@ -50,5 +74,7 @@
 </script>
 
 <style scoped>
-
+  .textField {
+    width: 300px;
+  }
 </style>
